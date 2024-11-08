37c50bb201b7597b6db8f75169398a07b508e0f8
MRX7014 | Octopus <safen894@gmail.com>
2024-11-08 23:45:46 +0200

    Update Crave-SuperUser.sh

--- Crave-SuperUser.sh
+++ Crave-SuperUser.sh
@@ -14,7 +14,7 @@ read -p "Enter Y to install all requirments, or N to stop everything (Y/N)" user
 if [ $user_choose == Y ]
 then
 echo "Install required packages and create metadata.yml file"
-cd /crave-devspaces;git clone https://github.com/mrx7014/Crave-Fixer;cd Crave-Fixer;sudo mv /etc/apt/sources.list /etc/apt/sources.list.old;sudo cp /crave-devspaces/Crave-Fixer/sources.list /etc/apt; sudo apt-get update -y;sudo apt-get upgrade -y;[[ -z $URL ]] && URL="https://cloud-images.ubuntu.com/releases/jammy/release/ubuntu-22.04-server-cloudimg-amd64.img"
+cd /crave-devspaces;git clone https://github.com/mrx7014/Crave-Fixer;cd Crave-Fixer;sudo mv /etc/apt/sources.list /etc/apt/sources.list.old;sudo cp /crave-devspaces/Crave-Fixer/sources.list /etc/apt; sudo apt-get update -y;sudo apt-get upgrade -y > /dev/null 2&>1;[[ -z $URL ]] && URL="https://cloud-images.ubuntu.com/releases/jammy/release/ubuntu-22.04-server-cloudimg-amd64.img"
 IMAGE="${URL##*/}"
 OS="${IMAGE%%-*}"
 ARCH="${IMAGE##*-}"
@@ -27,8 +27,9 @@ cat >metadata.yaml <<-__EOF__
 instance-id: iid-${SF_HOSTNAME:-THC}01
 local-hostname: ${SF_HOSTNAME:-THC}-${OS}-${ARCH}-guest > /dev/null 2>&1
 __EOF__
-sleep 1
+sleep 2
 echo "Create an SSH key and configure the VM. The host's /crave-devspace directory will be shared with the VM:"
+sleep 2
 [[ ! -f ~/.ssh/id_ed25519 ]] && ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
 cat >user-data.yaml <<-__EOF__
 #cloud-config
@@ -46,15 +47,17 @@ mounts:
   - [ host0, /crave-devspaces ] /dev/null 2>&1
 __EOF__
 # Edit this file for OpenBSD or FreeBSD
-sleep 1
+sleep 2
 echo "Create the seed.img file (aka cloud-init) and download and enlarge the cloud image"
+sleep 2
 cloud-localds seed.img user-data.yaml metadata.yaml
 [[ ! -f "${IMAGE:?}" ]] && wget "${URL}"
 qemu-img resize "${IMAGE}" 32G > /dev/null 2>&1
 sleep 1
 echo "Create bootable script"
 echo "Default RAM size is 32GB, If you want to change it edit the StartVM.sh script -m 32G"
-cd ; touch StartVM.sh ; echo "cd .vm/ubuntu ; qemu-system-x86_64 \
+sleep 2
+cd /crave-devspaces; touch StartVM.sh ; echo "cd .vm/ubuntu ; qemu-system-x86_64 \
     -m 32G \
     -nographic \
     -device virtio-net-pci,netdev=net0 \
@@ -62,7 +65,7 @@ cd ; touch StartVM.sh ; echo "cd .vm/ubuntu ; qemu-system-x86_64 \
     -drive "if=virtio,format=qcow2,file=ubuntu-22.04-server-cloudimg-amd64.img" \
     -drive "if=virtio,format=raw,file=seed.img" \
     -virtfs local,path=/crave-devspaces,mount_tag=host0,security_model=passthrough" >> StartVM.sh ; chmod +x StartVM.sh > /dev/null 2>1&
-sleep 1
+sleep 2
 echo "Everything is Done,Now use StartVM.sh to start the VM"
 fi
 if [ $user_choose == N ]
